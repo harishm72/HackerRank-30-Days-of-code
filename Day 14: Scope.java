3f0e85ff943ac19c20f9081f210bd9aabed1c004
Harish Chowdary  M <38683212+harishm72@users.noreply.github.com>
2018-11-02 13:03:16 +0530

    Update Day 14: Scope.java

--- Day 14: Scope.java
+++ Day 14: Scope.java
@@ -1,4 +1,15 @@
-	// Add your code here
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+
+// Add your code here
     Difference(int[] a){
         elements=new int[a.length];
         for(int i=0;i<a.length;i++){
@@ -10,3 +21,23 @@
         Arrays.sort(elements);
         maximumDifference=elements[elements.length-1]-elements[0];
     }
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
